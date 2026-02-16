2b4b8e6b26609d2e28f421d5bc21eba528c308ec
parth-ghelani22 <parthghelani04@gmail.com>
2026-02-16 20:30:05 +0530

    Add files via upload

---
+++ Unit 2/Pro_4.py
@@ -0,0 +1,16 @@
+#4.Create a module with 4 functions of your choice. Import and use the functions using module in different ways.
+
+import mymod
+
+a=5
+b=10
+
+addition=mymod.add(a,b)
+print(addition)
+subtraction=mymod.sub(a,b)
+print(subtraction)
+multiplication=mymod.mul(a,b)
+print(multiplication)
+division=mymod.div(a,b)
+print(division)
+
