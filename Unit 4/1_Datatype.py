63946062a144f4a20315bea3bdd5f634ecfde6e4
parth-ghelani22 <parthghelani04@gmail.com>
2026-03-27 12:03:01 +0530

    Update 1_Datatype.py

--- Unit 4/1_Datatype.py
+++ Unit 4/1_Datatype.py
@@ -1,3 +1,6 @@
+"""  Create an excel file with columns RollNo, Name, Gender, E-Mail, Mobile, Age and City. Enter atleast 20 records and then perform following exercise
+1) Write a program to load above excel file and display columns of file and data type of each column """
+
 import pandas as pd
 #loads file path
 file_path = r"D:\4053\Python\Unit 4"
